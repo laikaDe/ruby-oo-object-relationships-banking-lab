c0fcd084708bfe223b49b8c15544938fe0e77a50
Laika Deunert <laikadeunert@Laikas-Laptop.local>
2021-02-15 22:44:40 -0800

    commit tree d409403e3e1e4b0617ec0cd106b268bf323b28b8

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,36 @@
 class BankAccount
 
+    attr_accessor :balance, :status, :deposit, :transfer, :withdrawal, :amount
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+    end
+
+    def deposit(deposit)
+        #1. Can deposit money into account
+        @balance += deposit
+    end
+
+    def display_balance
+        @balance = balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        #1.Is valid with an open status and a balance greater than 0 
+        if self.balance > 0 && self.status == "open"
+            true 
+        else
+            false
+        end
+
+    end
+
+    def close_account 
+        @status = "closed"
+    end
+
 end
